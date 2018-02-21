5ceb333babf4561c59d2adc1fd4c875e459df1f5
falgunatara <33146340+falgunatara@users.noreply.github.com>
2018-02-21 23:12:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require ('pg').Pool;
+
 
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+    user:'falgunatara',
+    database:'falgunatara',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
+
+var pool = new Pool(config);
+
+app.get('/test-db',function(req,res){
+    
+    pool.query('SELECT * from test',function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.string(result.rows));
+        }
+    })
+    
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
